9d2ebc170aaf0c27d70c69f98927dfd4c38f2525
SurajGZ <surajg.zerovaega@gmail.com>
2022-05-22 23:12:58 +0530

    Demo HelloWorld

---
+++ src/add.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class add{
+    public static void main(String[] args){
+        int number1 = 10;
+        int number2 = 30;
+        int result;
+
+        result = number1 + number2;
+        System.out.println(result);
+    }
+}
