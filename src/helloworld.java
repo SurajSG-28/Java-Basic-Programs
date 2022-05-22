ceec2a7d4038ed2ae6362a58d5e0b2199e55bd9d
SurajGZ <surajg.zerovaega@gmail.com>
2022-05-22 23:09:10 +0530

    Demo HelloWorld

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 
 class helloworld {
         public static void main(String[] args) {
-                System.out.println("Hello, World!");
+                System.out.println("Hello, Suraj SG!");
         }
 }
