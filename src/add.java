870642ded12db5ebaac9182240cdf980617f1f25
SurajGZ <surajg.zerovaega@gmail.com>
2022-05-22 23:21:25 +0530

    added addition using Scanner

--- src/add.java
+++ src/add.java
@@ -8,6 +8,19 @@ class add{
         int result;
 
         result = number1 + number2;
-        System.out.println(result);
+        System.out.println("Inline :- "+result);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number 1");
+        int sc_number_1 = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter Number 2");
+        int sc_number_2 = Integer.parseInt(sc.nextLine());
+
+        int sc_result = sc_number_1 + sc_number_2;
+
+        System.out.println("Using Scanner :- "+sc_result);
+
+
     }
 }
